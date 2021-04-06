a31d2833a0457f287c1b2d222285663fb7243ae9
Alejandro <alejpr@kth.se>
2021-04-06 18:34:47 +0200

    Color change + express server

--- server.js
+++ server.js
@@ -1,52 +1,19 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+var express = require("express");
+var path = require("path");
+var app = express();
 
-http.createServer(function (request, response) {
-    // console.log('request starting...');
-    var filePath = '.' + request.url;
-    if (filePath == './')
-        filePath = './index.html';
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
-    }
-    fs.readFile(filePath, function (error, content) {
-        if (error) {
-            if (error.code == 'ENOENT') {
-                fs.readFile('./404.html', function (error, content) {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
-                response.end();
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
-}).listen(8000);
-console.log('Server running at http://127.0.0.1:8000/');
+const PORT = 8000;
+
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.use('/', (req, res) => {
+	res.sendFile(path.join(__dirname, req.path));
+});
+
+
+app.listen(PORT, () => {
+	console.log(`Server started at ${PORT}`);
+});
